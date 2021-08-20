a86807eddd1f0c11a6274a9f46aa88d68c0f2a2c
mfruzan <48665672+mfruzan@users.noreply.github.com>
2021-08-20 16:22:02 +0930

    Add files via upload

--- comparison2.R
+++ comparison2.R
@@ -1,3 +1,4 @@
+accepted <- c("0|1", "1|0", "1|2", "2|1")
 sswap <- function(str){
   lst <- strsplit(str,"\\|")
   s1 <- lst[[1]][1]
@@ -15,14 +16,14 @@ simcode <- function(str){
   #otherwise
   return(3)
 }
-indblocks <- function(v, el){
+findblocks <- function(v, el){
   #in vector v finds index  of blocks of el
   idx <-numeric(length=0)
   block_start <- -1
   for(i in 1:length(v)){
     if (v[i]==el){
       if (block_start == -1)
-        block_start <- i
+         block_start <- i
     }else{
       if (block_start != -1){
         idx <- c(idx,block_start)
@@ -38,7 +39,7 @@ indblocks <- function(v, el){
 }
 switcherror <- function(v){
   vv <- sapply(v, FUN=simcode)  
-  #count number of blocks of 1,2 and 3
+   #count number of blocks of 1,2 and 3
   #example if we have 11122111 then assuming phase switch error will be 1, assuming opposite-phase then switch error will be 2, so the final switch error is min(1,2)=1
   #assuming phase with reference then we count blocks of 2 and 3 (none 1):
   blocks1 <- findblocks(vv,1)
@@ -51,6 +52,9 @@ switcherror <- function(v){
   
   return (min(length(phased), length(unphased)))
 }
+
+
+
 n50 <- function(contig_lens){
   contig_lens <- sort(contig_lens, decreasing = T)
   n50_value <- 0.5 * sum(contig_lens);
@@ -59,118 +63,57 @@ n50 <- function(contig_lens){
     if (cum_lens[i]>= n50_value)
       return (contig_lens[i])
   }
+  
   return (-1)
+  
 }
-dfvcf <- read.table("commonmerge_noheader.vcf", header = F, colClasses = c("character","numeric","NULL","NULL","NULL","NULL","NULL","NULL","NULL","character"))
+
+dfvcf<-read.table("NA12878_sorted_noheader.vcf", header=F, colClasses=c("character","numeric","NULL","NULL","NULL","NULL","NULL","NULL","NULL","character"))
+#only keep het positiosn
+dfvcf <- dfvcf[dfvcf$V10 %in% c('1|0','0|1','1|2','2|1','0|2','2|0'),]
 dfvcf$snp_num <- c(1:nrow(dfvcf))
 dim(dfvcf)
-#1,890,198  4
-############################## HaploMiner
-dfhaplominer <-read.table("haplominer_commonmerge.hap", colClasses = c("character", "numeric","character","character","character","character"), header = F, fill = T)
+#2,490,062  4
+dfhaplominer <-read.table("haplominer_pacbio.hap", colClasses=c("character", "numeric","character","character","character","character") ,header=F, fill=T)
 dfhaplominer[dfhaplominer=='']<-NA
-dfhaplominer_rows <- dfhaplominer[!is.na(dfhaplominer$V6), ]
+head(dfhaplominer)
+dfhaplominer_rows <-dfhaplominer[!is.na(dfhaplominer$V6), ]
 factors <- dfhaplominer[is.na(dfhaplominer$V6), 4]
 dfhaplominer_rows$rownum <- c(1:nrow(dfhaplominer_rows))
 dfmerged <-merge(dfhaplominer_rows,dfvcf, by=c(1,2), all.x=T)
 dfmerged <- dfmerged[order(dfmerged$rownum),]
-dup_idx <- which(duplicated(dfmerged$rownum))
-dfmerged <- dfmerged[-dup_idx,]
 rowfactor <- rep(c(1:length(factors)), factors)
 dfmerged$rowfactor <- rowfactor
 dfmerged$both <- paste(dfmerged$V6,'_',dfmerged$V10,sep="")
-agg <- aggregate(both ~ rowfactor, dfmerged, FUN=function(b)switcherror(b))
-# Estimate N50
+agg<-aggregate(both~rowfactor, dfmerged, FUN=function(b){switcherror(b)})
+
 tmp <- dfhaplominer[is.na(dfhaplominer$V6), 2:3]
 haplominer_lens <- as.numeric(tmp$V3)-as.numeric(tmp$V2) + 1
 n50(haplominer_lens)
-#326
+#46,787 
+sum(haplominer_lens)
+# 1,728,491,117
+length(haplominer_lens)
+#[1] 67,809
 mean(haplominer_lens)
-# 251
+# 25,490
 max(haplominer_lens)
-# 2770
+#351,891 
 
-now estimate haplominer swtich rate per kbp of long haplotype
+###########################################
+#now estimate haplominer swtich rate per kbp of long haplotype
 length(which(haplominer_lens<1000))
-#320142
+#3477
 length(which(haplominer_lens>=1000))
-#2318
+#64332
 agg$len_bp <- haplominer_lens
 agg_long <- agg[agg$len_bp>=2,]
 (sum(agg_long$both)*1000)/sum(agg_long$len_bp)
-#0.0321 
-############################### HapCompass
-dfhapcompass <- read.table("hapcompass_commonmerge_q5.hap", colClasses=c("character","numeric","numeric","numeric","numeric","NULL","character"), header=F, fill=T)
-#1,173,435
-dfhapcompass[dfhapcompass=='']<-NA 
-summary_vec <-which(!is.na(dfhapcompass$V7))
-summary <-dfhapcompass[which(dfhapcompass$V1=="BLOCK"),c("V2","V3")]
-hapcompass_lens <- summary$V3 - summary$V2 +1 
-factors2<- c(summary_vec[-1], (length(dfhapcompass$V1)+1)) - summary_vec - 1
-chr_vec <- dfhapcompass$V7[!is.na(dfhapcompass$V7)]
-dfhapcompass_rows <- dfhapcompass[is.na(dfhapcompass$V7) ,1:5]
-dfhapcompass_rows$V1 <- rep(chr_vec, factors2)
-dfhapcompass_rows$V8 <- paste(dfhapcompass_rows$V4,"|", dfhapcompass_rows$V5, sep="")
-dfhapcompass_rows$rownum <- c(1:nrow(dfhapcompass_rows))
-dfmerged2 <-merge(dfhapcompass_rows,dfvcf, by=c(1,2), all.x=T)
-dfmerged2 <- dfmerged2[order(dfmerged2$rownum),]
-dup_idx <- which(duplicated(dfmerged2$rownum))
-dfmerged2 <-dfmerged2[-dup_idx,]
-rowfactor <- rep(c(1:length(factors2)), factors2)
-dfmerged2$rowfactor <- rowfactor
-dfmerged2$both <- paste(dfmerged2$V8,'_',dfmerged2$V10,sep="")
-agg2<-aggregate(both~rowfactor, dfmerged2, FUN=function(b){switcherror(b)})
-#now estimate hapcompass swtich rate per kbp of long haplotype
-length(which(hapcompass_lens<1000))
-#330295
-length(which(hapcompass_lens>=1000))
-#2829
-agg2$len_bp <- hapcompass_lens
-agg2_long <- agg2[agg2$len_bp>=2,]
-(sum(agg2_long$both)*1000)/sum(agg2_long$len_bp)
-#0.1439
-
-# As HapCompass reports non-continuous SNPs as haplotype: we require code 
-# to detect only continuous ones
-v<-dfhapcompass_rows$V3
-series<-numeric(length=0)
-hapcompass_lens <- numeric(length=0)
-start_idx <- 1
-for(fact in factors2){
-  
-  split_vec <- numeric(length=0)
-  for (i in 0:(fact-2)){
-    if(v[start_idx+i+1]-v[start_idx+i] >1){
-      split_vec <- c(split_vec,i)
-    }
-  }
-  if(length(split_vec)==0){
-    series <- c(series, fact)
-    hapcompass_lens <- c(hapcompass_lens, dfhapcompass_rows[start_idx + fact-1,"V2"]- dfhapcompass_rows[start_idx,"V2"] + 1)
-  }else{
-    last <- -1
-    s_idx <- start_idx
-    for(s in split_vec){
-      series <- c(series, s - last)
-      hapcompass_lens <- c(hapcompass_lens, dfhapcompass_rows[s_idx + s - last-1,"V2"]- dfhapcompass_rows[s_idx,"V2"] + 1)
-      s_idx <- s_idx + s - last
-      last <- s
-      
-      
-    }     
-    series <- c(series, fact - last -1)
-    hapcompass_lens <- c(hapcompass_lens, dfhapcompass_rows[s_idx + fact - last -2,"V2"]- dfhapcompass_rows[s_idx,"V2"] + 1)
-  }
-  start_idx <- start_idx + fact
-}
-# Estimate N50
-n50(hapcompass_lens)
-# 329
-mean(hapcompass_lens)
-# 246
-max(hapcompass_lens)
-# 2770
-############################### HapCUT2
-dfhapcut <- read.table("hapcut2_commonmerge_q5.hap",sep = "\t" , fill=T)
+#0.0274 Logic9
+##########################################################################
+############################################################################
+#hapcut2
+dfhapcut <- read.table("hapcut2_pruned_pacbio.hap",sep = "\t" , fill=T)
 dfhapcut[dfhapcut=='']<-NA
 dfhapcut_rows <- dfhapcut[!is.na(dfhapcut$V2) ,c(4,5,2,3)]
 dfhapcut_rows$V4 <- as.character(dfhapcut_rows$V4)
@@ -180,17 +123,17 @@ factors3<-  summary_vec - c(0, summary_vec[-length(summary_vec)])  - 2
 factors3 <- c(factors3,2)
 sum(factors3)
 nrow(dfhapcut_rows)
-#667676
+#2,132,787
 dfhapcut_rows$V6 <- paste(dfhapcut_rows$V2,"|", dfhapcut_rows$V3, sep="")
 dfhapcut_rows$rownum <- c(1:nrow(dfhapcut_rows))
 dfmerged3 <-merge(dfhapcut_rows,dfvcf, by=c(1,2), all.x=T)
 dfmerged3 <- dfmerged3[order(dfmerged3$rownum),]
 dup_idx <- which(duplicated(dfmerged3$rownum))
 rowfactor <- rep(c(1:length(factors3)), factors3)
+rowfactor <- c(rowfactor, rowfactor[length(rowfactor)])
 dfmerged3$rowfactor <- rowfactor
 dfmerged3$both <- paste(dfmerged3$V6,'_',dfmerged3$V10,sep="")
 agg3<-aggregate(both~rowfactor, dfmerged3, FUN=function(b){switcherror(b)})
-
 #work out hapcut_lens
 hapcut_lens <- numeric(length=0)
 sidx <- 0
@@ -199,19 +142,18 @@ for(hl in factors3){
   hapcut_lens <- c(hapcut_lens, tmp)
   sidx <- sidx + hl
 }
-
+###########################################
 #now estimate hapcut swtich rate per kbp of long haplotype
 length(which(hapcut_lens<1000))
-#270,488
+#1025
 length(which(hapcut_lens>=1000))
-#1721
+#23,262
 agg3$len_bp <- hapcut_lens
 agg3_long <- agg3[agg3$len_bp>=2,]
 (sum(agg3_long$both)*1000)/sum(agg3_long$len_bp)
-#0.0376
-
-# As HapCUT2 reports non-continuous SNPs as haplotype: we require code 
-# to detect only continuous ones
+#0.0058
+#############################################
+#because hapcut2 reports non-continuous SNP as haplotype: we write a code to detect only conitinuous ones:
 v<-dfmerged3$snp_num
 series3<-numeric(length=0)
 hapcut_lens <- numeric(length=0)
@@ -243,17 +185,24 @@ for(fact in factors3){
   }
   start_idx <- start_idx + fact
 }
-# Estimate N50
 n50(hapcut_lens)
-# 313
+#10,804
+sum(hapcut_lens)
+#1,426,036,974
 mean(hapcut_lens)
-# 224
+#4802.344
 max(hapcut_lens)
-# 2581
+#118,473
+max(series3)
+#178
+length(hapcut_lens)
+#296,946
 
-############################### WhatsHap
+###########################################################################################################
+###########################################################################################################
+#WhatsHap
 
-dfwhatshap<-read.table("whatshap_phased_commonmerge.vcf", header=F, colClasses=c("character","numeric","NULL","NULL","NULL","NULL","NULL","NULL","NULL","character"))
+dfwhatshap<-read.table("whatshap_pacbio.vcf", header=F, colClasses=c("character","numeric","NULL","NULL","NULL","NULL","NULL","NULL","NULL","character"))
 hap.list <- list()
 haplotype_started <- F
 last_pos <- 0 # last position
@@ -275,6 +224,8 @@ for(i in 1: nrow(dfwhatshap)){
       haplotype_num <- haplotype_num + 1
     }
     # now write haplotype line 
+    #dfwhatshap_rows <- rbind(dfwhatshap_rows, list(chr=dfwhatshap[i,"V1"], pos=dfwhatshap[i,"V2"], GN=ss[[1]][1], rowfactor=haplotype_num))
+    #dfwhatshap_rows[cnt,] <- list(dfwhatshap[i,"V1"],dfwhatshap[i,"V2"],ss[[1]][1],haplotype_num)
     hap.list[[cnt]] <- list(dfwhatshap[i,"V1"],dfwhatshap[i,"V2"],ss[[1]][1],haplotype_num)
     cnt <- cnt + 1
     last_start_pos <- start_pos
@@ -282,7 +233,7 @@ for(i in 1: nrow(dfwhatshap)){
     haplotype_started <- F
   }
   last_pos <- dfwhatshap[i,"V2"]
-
+  
 }
 dfhwhathaps_rows <- do.call("rbind", hap.list) # this will return a matrix
 dfhwhathaps_rows <- as.data.frame(dfhwhathaps_rows)
@@ -292,21 +243,18 @@ dfhwhathaps_rows$V3 <- as.character(dfhwhathaps_rows$V3)
 dfhwhathaps_rows$V4 <- as.integer(dfhwhathaps_rows$V4)
 colnames(dfhwhathaps_rows)[4] <- "rowfactor"
 dim(dfhwhathaps_rows)
-#[1] 828,878      4
-#then we realize that whatshap reports many haplotypes with length 1 SNP!!!
+#[1] 2,445,164      4
 hap_lens<-aggregate(V1~rowfactor, dfhwhathaps_rows, FUN=length)
 length(which(hap_lens$V1==1))
-#[1] 8957  haplotypes are with length 1
+#[1] 150,644  haplotypes are with length 1
 #now filter rows that contain hap with length 1
 dfhwhathaps_rows <- dfhwhathaps_rows[!dfhwhathaps_rows$rowfactor %in% hap_lens[hap_lens$V1==1,"rowfactor"] , ]
 dim(dfhwhathaps_rows)
-# 815,976      4
+# 2,294,520      4
 dfhwhathaps_rows$rownum <- c(1:nrow(dfhwhathaps_rows))
 dfmerged4 <-merge(dfhwhathaps_rows,dfvcf, by=c(1,2), all.x=T)
 dfmerged4 <- dfmerged4[order(dfmerged4$rownum),]
 dup_idx <- which(duplicated(dfmerged4$rownum))
-#rowfactor <- rep(c(1:length(factors3)), factors3)
-#dfmerged3$rowfactor <- rowfactor
 dfmerged4$both <- paste(dfmerged4$V3,'_',dfmerged4$V10,sep="")
 
 # As WhatsHap reports non-continuous SNPs as haplotype: we require code 
@@ -346,21 +294,41 @@ for(fact in agg4_len$both){
 }
 
 n50(whatshap_lens)
-#329
+#14,828
+sum(whatshap_lens)
+#1,230,910,625
 mean(whatshap_lens)
-#252
+#4737
+min(whatshap_lens)
+#2
 max(whatshap_lens)
-#2770
+#265,020
 
-#after splitting based on continuous SNP, some haplotype with length 1 will be generated, we remove them again from dfmerged4
+sum(series4)
+#[1] 2,294,520
+max(series4)
+#423
+table(series4)
+length(which(series4==1))
+#[1] 0
+#after splitting based on coninuous SNP, some haplotype with length 1 will generated, we remove them again from dfmerged4
 
 dfmerged4$rowfactor <- rep(1:length(series4), series4)
 dfmerged4$hlen <- rep(series4, series4)
+dfmerged4$hlen_bp <- rep(whatshap_lens, series4)
 dfmerged4 <- dfmerged4[dfmerged4$hlen>1,]
+dim(dfmerged4) 
+#[1] 2,294,520      10
 #now calculate accuracy 
 agg4<-aggregate(both~rowfactor, dfmerged4, FUN=function(b){switcherror(b)})
+
 #find switch error rate per kbp for all haplotypes
 agg4$len_bp <- whatshap_lens
 agg4_long <- agg4[agg4$len_bp>=2,]
 (sum(agg4_long$both)*1000)/sum(agg4_long$len_bp)
-#0.0842
+#0.0207
+length(which(whatshap_lens<1000))
+#100430
+length(which(whatshap_lens>=1000))
+#159368
+
